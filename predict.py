52df518910c0572dea293a0da761cc27791e4862
gas <gasparian>
2018-12-23 16:19:36 +0500

    prediction fixed

--- predict.py
+++ predict.py
@@ -18,6 +18,8 @@ start = time.time()
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--data_path', type=str, required=True)
 parser.add_argument('--model_path', type=str, required=True)
+parser.add_argument('--delay', type=int, default=7, required=False)
+parser.add_argument('--denoise_borders', action='store_true')
 parser.add_argument('--gpu', type=int, default=-1, required=False)
 args = parser.parse_args()
 globals().update(vars(args))
@@ -38,18 +40,18 @@ if files_list:
             img = cv2.imread(data_path+"/"+fname)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img = np.array(img, dtype=np.uint8)
-            out = trainer.predict_mask(img, biggest_side=None, denoise_borders=False)
+            out = trainer.predict_mask(img, biggest_side=None, denoise_borders=denoise_borders)
             cv2.imwrite('%s/%s_seg.png' % (data_path, fname.split(".")[0]), out[0])
         print(" [INFO] Images processed! ")
 
     if vids:
         for fname in vids:
             imgs = split_video(data_path+"/"+fname, frame_rate=12)
-            out = trainer.predict_mask(imgs, biggest_side=None, denoise_borders=False)
+            out = trainer.predict_mask(imgs, biggest_side=None, denoise_borders=denoise_borders)
             vpath = data_path+"/%s" % fname.split(".")[0]
             os.mkdir(vpath)
             save_images(out, path=vpath)
-            os.system(f"convert -delay 7 -loop 0 -dispose Background {vpath}/*.png {vpath}/{fname.split('.')[0]}.gif")
+            os.system(f"convert -delay {delay} -loop 0 -dispose Background {vpath}/*.png {vpath}/{fname.split('.')[0]}.gif")
         print(" [INFO] Videos processed! ")
 
 print(" [INFO] %s ms. " % round((time.time()-start)*1000, 0))
