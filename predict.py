e6f0db24da6ffc7d0247ee1260a566b05b7d3df3
gas <gasparian>
2018-12-23 20:40:51 +0500

    prediction fixed 2

--- predict.py
+++ predict.py
@@ -11,7 +11,7 @@ import cv2
 
 from utils import *
 
-# python3 predict.py -p ./test --model_path ./models/model --gpu -1 --frame_rate 12 --denoise_borders
+# python3 predict.py -p ./test --model_path ./models/model --gpu -1 --frame_rate 12 --denoise_borders --biggest_side 512
 
 start = time.time()
 
@@ -19,13 +19,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--data_path', type=str, required=True)
 parser.add_argument('--model_path', type=str, required=True)
 parser.add_argument('--gpu', type=int, default=-1, required=False)
+parser.add_argument('--biggest_side', type=int, default=0, required=False)
 parser.add_argument('--delay', type=int, default=7, required=False)
 parser.add_argument('--frame_rate', type=int, default=12, required=False)
 parser.add_argument('--denoise_borders', action='store_true')
 args = parser.parse_args()
 globals().update(vars(args))
 
+biggest_side = None if not biggest_side else biggest_side
 delay = round(100/frame_rate + .5)
+
 trainer = Trainer(path=model_path, gpu=gpu)
 trainer.load_state(mode="metric")
 
@@ -42,14 +45,14 @@ if files_list:
             img = cv2.imread(data_path+"/"+fname)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img = np.array(img, dtype=np.uint8)
-            out = trainer.predict_mask(img, biggest_side=None, denoise_borders=denoise_borders)
+            out = trainer.predict_mask(img, biggest_side=biggest_side, denoise_borders=denoise_borders)
             cv2.imwrite('%s/%s_seg.png' % (data_path, fname.split(".")[0]), out[0])
         print(" [INFO] Images processed! ")
 
     if vids:
         for fname in vids:
             imgs = split_video(data_path+"/"+fname, frame_rate=frame_rate)
-            out = trainer.predict_mask(imgs, biggest_side=None, denoise_borders=denoise_borders)
+            out = trainer.predict_mask(imgs, biggest_side=biggest_side, denoise_borders=denoise_borders)
             vpath = data_path+"/%s" % fname.split(".")[0]
             os.mkdir(vpath)
             save_images(out, path=vpath)
